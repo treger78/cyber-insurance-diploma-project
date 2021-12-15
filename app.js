e5a1c6c1838ac8072cc2d6b65ead5bc2681d4c58
treger78 <ilya.pustograev.i@mail.ru>
2021-12-15 20:13:25 +0300

    fix: fix brackets

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api/estate', require('./routes/estate.routes'));
 app.use('/api/health', require('./routes/health.routes'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname), 'client', 'build'));
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
