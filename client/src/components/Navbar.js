9f375726300de3d7eb1c44b3002f6b446e8830dc
treger78 <ilya.pustograev.i@mail.ru>
2021-12-16 12:34:19 +0300

    fix: fix navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -12,8 +11,8 @@ export const Navbar = () => {
     <header>
       <nav>
         <div className="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
-          <NavLink to="/" className="brand-logo">CI</NavLink>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <span className="brand-logo"><NavLink to="/">CI</NavLink></span>
+          <NavLink to="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></NavLink>
           <ul className="right hide-on-med-and-down">
             <li><NavLink to="/">Главная</NavLink></li>
             <li><NavLink to="/vehicle">Транспортные средства</NavLink></li>
@@ -26,12 +25,12 @@ export const Navbar = () => {
       </nav>
 
       <ul className="sidenav" id="mobile-demo">
-        <li><NavLink to="/">Главная</NavLink></li>
-        <li><NavLink to="/vehicle">Транспортные средства</NavLink></li>
-        <li><NavLink to="/trips">Путешествия</NavLink></li>
-        <li><NavLink to="/estate">Имущество</NavLink></li>
-        <li><NavLink to="/health">Здоровье</NavLink></li>
-        <li><NavLink to="/personal">Личный кабинет</NavLink></li>
+          <li><NavLink to="/">Главная</NavLink></li>
+          <li><NavLink to="/vehicle">Транспортные средства</NavLink></li>
+          <li><NavLink to="/trips">Путешествия</NavLink></li>
+          <li><NavLink to="/estate">Имущество</NavLink></li>
+          <li><NavLink to="/health">Здоровье</NavLink></li>
+          <li><NavLink to="/personal">Личный кабинет</NavLink></li>
       </ul>
     </header>
   );
